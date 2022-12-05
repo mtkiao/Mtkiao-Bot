79acc2bd093bf1ee1be903e4f8093718f92cfd82
litesans <a0979511006@gmail.com>
2022-12-05 23:13:12 +0800

    更新兼容性

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const { token } = require("./Data/data.json")
 const axios = require('axios')
 
 axios.get('https://discord.com', {
-}).then((response) => {
-  console.log("網路連線正常:", response.status)
-}).catch((request) => {
-  console.log("網路連線異常:", request.response.status)
-})
+  }).then((response) => {
+    console.log("網路連線正常:", response.status)
+  }).catch((request) => {
+    console.log("網路連線異常:", request.response.status)
+  })
 
 const client = new Client({
   intents: [
@@ -22,32 +22,56 @@ client.antinuke = {};
 client.antispam = {};
 client.cooldowns = new Discord.Collection();
 client.COOLDOWN_SECONDS = 50;
-// client.antinuke = new Discord.Collection();
-// client.antinuke_second = 10;
 
 client.commands = new Collection();
 const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-const eventsFiles = fs.readdirSync("./events").filter(file => file.endsWith(".js"));
+const evebtsPath = path.join(__dirname, 'events');
 
-for (const file of commandFiles) {
-  const filePath = path.join(commandsPath, file);
-  const command = require(filePath);
-  client.commands.set(command.data.name, command);
+function ReadFiles(Path) {
+  if (!fs.existsSync(Path)) return []
+
+  let AllFiles = []
+  const Files = fs.readdirSync(Path)
+  for (const file of Files) {
+      const filePath = path.join(Path, file)
+      const fileStat = fs.lstatSync(filePath)
+      if (fileStat.isDirectory()) {
+          AllFiles = AllFiles.concat(ReadFiles(path.join(Path, file)))
+      }
+      if (filePath.endsWith('.js')) {
+          AllFiles.push(filePath)
+      }
+  }
+  return AllFiles
 }
-const commands = [];
-for (const file of eventsFiles) {
-  const event = require(`./events/${file}`);
+
+// Load commands
+for (const filePath of ReadFiles(commandsPath)) {
+  try {
+    const command = require(filePath);
+    client.commands.set(command.data.name, command);
+    console.log('\033[40;32m Successfully loaded command \033[42;32m' + command.data.name + '\033[0m')
+  } catch {
+    console.log('\033[40;31m Failed to load command \033[41;31m' + filePath + '\033[0m')
+  }
+}
+
+// Load events
+const commands = []
+for (const filePath of ReadFiles(evebtsPath)) {
+  const event = require(filePath);
   if (event.once) {
     client.once(event.name, (...args) => event.execute(client, ...args, commands))
   }
   else {
     client.on(event.name, (...args) => event.execute(client, ...args, commands))
   }
+  console.log('\033[40;32m Successfully loaded event \033[42;32m' + event.name + '\033[0m')
+
 }
 
 client.once('ready', async () => {
-  console.log('Ready!');
+  console.log('Ready! Logged in as ' + client.user.tag);
   const { sleep } = require("./Command_Library/wait.js")
   while (1) {
     client.user.setPresence({
